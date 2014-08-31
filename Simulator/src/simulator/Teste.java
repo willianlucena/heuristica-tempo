ab1a4f574366362a0646d7e7d2e2bf9b468403eb
jwillian.uern <jwillian.uern@gmail.com>
2014-08-31 06:18:49 +0000

    Exibindo label no start

--- Simulator/src/simulator/Teste.java
+++ Simulator/src/simulator/Teste.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package simulator;
 
 /**
@@ -11,21 +10,21 @@ package simulator;
  * @author extra
  */
 public class Teste {
-    
+
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
         long end = start;
-        
-        System.out.println("start "+start);
-        System.out.println("end "+end);
-        System.out.println("total "+(end - start));
-        
+
+        System.out.println("start " + start);
+        System.out.println("end " + end);
+        System.out.println("total " + (end - start));
+
         while ((end - start) < 5000l) {
             end = System.currentTimeMillis();
-            System.out.println("start "+start);
-            System.out.println("end "+end);
-            System.out.println("total "+(end - start));
+            System.out.println("start " + start);
+            System.out.println("end " + end);
+            System.out.println("total " + (end - start));
         }
-        
+
     }
 }
