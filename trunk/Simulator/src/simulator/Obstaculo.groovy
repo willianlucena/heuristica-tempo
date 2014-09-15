8cc30a6d2757549ea23afb48b9b7452397f075ef
jwillian.uern <jwillian.uern@ca288b96-5aa5-b6db-b342-90b2688b3d85>
2014-09-15 23:05:43 +0000

    Alterado os códigos de Java para Groovy, afim de dar mais dinamica nos testes e na programação. Já está lendo o arquivo padrão com o modelo de grid. Fazer: ao ler o arquivo, identificar os caracteres que representam cada tipo e ator, e ir populando um mapa com valores inteiros que representaram o grid lido do arquivo texto.

---
+++ trunk/Simulator/src/simulator/Obstaculo.groovy
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simulator;
+
+import ch.aplu.jgamegrid.Actor;
+
+/**
+ *
+ * @author willian
+ */
+public class Obstaculo extends Actor {
+
+    public Obstaculo() {
+        super("sprites/stone.gif", 2); // Two sprites
+    }
+}
---
+++ trunk/Simulator/src/simulator/Tarefa.groovy
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simulator;
+
+import ch.aplu.jgamegrid.Actor;
+
+/**
+ *
+ * @author willian
+ */
+public class Tarefa extends Actor {
+
+    public Tarefa() {
+        super("sprites/target.gif");
+    }
+}
