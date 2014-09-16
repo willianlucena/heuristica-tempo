175e1f0dda61a16a9e3293001e99da570f5e4536
jwillian.uern <jwillian.uern@ca288b96-5aa5-b6db-b342-90b2688b3d85>
2014-09-16 20:35:58 +0000

    Voltei para Java, pois o código apresentava erro, possivelmente por causa do JGameGrid que não entende direito o Groovy.
    Quando precisar fazer códigos mais complicados, irei criar os métodos em classes groovy.

---
+++ trunk/Simulator/src/simulator/Main.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simulator;
+
+import java.awt.EventQueue;
+
+/**
+ *
+ * @author extra
+ */
+public class Main {
+
+    public Main() {
+        /*
+         String gridTeste =
+         "    xxxxx          \n" + // 0 (19)
+         "    x...x          \n" + // 1
+         "    x*..x          \n" + // 2
+         "  xxx..*xx         \n" + // 3
+         "  x..*.*.x         \n" + // 4
+         "xxx.x.xx.x   xxxxxx\n" + // 5
+         "x...x.xx.xxxxx..oox\n" + // 6
+         "x.*..*..........oox\n" + // 7
+         "xxxxx.xxx.xAxx..oox\n" + // 8
+         "    x.....xxxxxxxxx\n" + // 9
+         "    xxxxxxx        \n";  //10
+         */
+        Ambiente ambiente = new Ambiente(1);
+        ambiente.init();
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Main main = new Main();
+            }
+        });
+    }
+}
