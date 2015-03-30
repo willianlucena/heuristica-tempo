0354498f0b5e9e892a5d4e73e3b6f0aae5d0e3c8
jwillian.uern <jwillian.uern@ca288b96-5aa5-b6db-b342-90b2688b3d85>
2015-03-30 22:34:21 +0000

    Inserindo robôs e tarefas aleatoriamente no ambiente quando marcada a opção para tal ação. Falta inserir o algoritmo para designar o caminho de cada robô para cada tarefa. Após isso, usar essa informação de distância junto com a escolha do robô pela heurística de cenários com janelas de tempo, para eleger o robô mais apto a realizar cada tarefa.

--- trunk/Simulator/src/simulator/ConfiguracaoAmbiente.java
+++ trunk/Simulator/src/simulator/ConfiguracaoAmbiente.java
@@ -124,43 +124,46 @@ public class ConfiguracaoAmbiente extends javax.swing.JFrame {
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
-                jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGap(0, 247, Short.MAX_VALUE)
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 276, Short.MAX_VALUE)
         );
         jPanel2Layout.setVerticalGroup(
-                jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGap(0, 0, Short.MAX_VALUE)
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
         );
-        jPanel2.add(gridConfig);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         layout.setVerticalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 243, Short.MAX_VALUE)
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
 
-        for (int i = 0; i < gridConfig.getQtdRobos(); i++) {
-            Robot robot = gridConfig.getRobos().get(i);
-            gridConfig.addRoboRandom(robot);
+        if (gridConfig.robosRandom) {
+            for (int i = 0; i < gridConfig.getQtdRobos(); i++) {
+                Robot robot = gridConfig.getRobos().get(i);
+                gridConfig.addRoboRandom(robot);
+            }
+            gridConfig.setContDeployRobot(gridConfig.getQtdRobos());
         }
-        gridConfig.setContDeployRobot(gridConfig.getQtdRobos());
 
-        for (int i = 0; i < gridConfig.getQtdTarefas(); i++) {
-            Tarefa tarefa = gridConfig.getTarefas().get(i);
-            gridConfig.addTarefaRandom(tarefa);
+        if (gridConfig.tarefasRandom) {
+            for (int i = 0; i < gridConfig.getQtdTarefas(); i++) {
+                Tarefa tarefa = gridConfig.getTarefas().get(i);
+                gridConfig.addTarefaRandom(tarefa);
+            }
+            gridConfig.setContDeployTask(gridConfig.getQtdTarefas());
         }
-        gridConfig.setContDeployTask(gridConfig.getQtdTarefas());
 
         for (int i = 0; i < gridConfig.getQtdObstaculos(); i++) {
             gridConfig.addObstaculoRandom(new Obstaculo());
@@ -261,7 +264,7 @@ public class ConfiguracaoAmbiente extends javax.swing.JFrame {
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 247, Short.MAX_VALUE)
+            .addGap(0, 276, Short.MAX_VALUE)
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -279,7 +282,7 @@ public class ConfiguracaoAmbiente extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 243, Short.MAX_VALUE)
             .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
